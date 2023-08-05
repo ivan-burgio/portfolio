8a9c11cc33b93008c5dc646aaadbf238e3515d7f
ivan-burgio <ivan.24.burgio@gmail.com>
2023-08-04 22:25:10 -0300

    Comienzo pagina del inicio

--- views/inicio/principal.php
+++ views/inicio/principal.php
@@ -1,4 +1,9 @@
 <header class="header">
-    <h1 class="fuente_headings">Portafolio</h1>
+    <nav class="navegacion"></nav>
 
+    <div class="bienvenida contenedor-sm">
+        <h1>Welcome to my portfolio!</h1>
+        <h2>I´m Iván Burgio</h2>
+        <p class="descripcion-pagina">I´m a FullStack Web Development student who really enjoys learning about programming.</p>
+    </div>
 </header>
