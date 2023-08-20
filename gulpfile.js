7bdbc80801d033b094b356e1000c853ecc42d924
ivan-burgio <ivan.24.burgio@gmail.com>
2023-08-20 20:08:29 -0300

    arreglo de dependencias

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,86 @@
-const { src, dest, watch , series, parallel } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
+
+// CSS
 const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 const autoprefixer = require('autoprefixer');
-const postcss    = require('gulp-postcss')
-const sourcemaps = require('gulp-sourcemaps')
 const cssnano = require('cssnano');
-const concat = require('gulp-concat');
-// const terser = require('gulp-terser-js');
-const rename = require('gulp-rename');
-const imagemin = require('gulp-imagemin'); // Minificar imagenes 
-const notify = require('gulp-notify');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+
+// Imagenes
 const cache = require('gulp-cache');
-const clean = require('gulp-clean');
+const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+// Javascript
+const terser = require('gulp-terser-js');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename')
+
 
 const paths = {
     scss: 'src/scss/**/*.scss',
     js: 'src/js/**/*.js',
     imagenes: 'src/img/**/*'
 }
-
 function css() {
     return src(paths.scss)
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(postcss([autoprefixer(), cssnano()]))
-        // .pipe(postcss([autoprefixer()]))
-        .pipe(sourcemaps.write('.'))
-        .pipe( dest('public/build/css') );
+        .pipe( sourcemaps.init())
+        .pipe( sass({outputStyle: 'expanded'}))
+        .pipe( postcss([autoprefixer(), cssnano()]))
+        .pipe( sourcemaps.write('.'))
+        .pipe(  dest('public/build/css') );
 }
-
-
 function javascript() {
     return src(paths.js)
-      //.pipe(terser())
+      .pipe(sourcemaps.init())
+      .pipe(concat('bundle.js')) 
+      .pipe(terser())
       .pipe(sourcemaps.write('.'))
-      .pipe(dest('public/build/js'));
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(dest('./public/build/js'))
 }
 
 function imagenes() {
     return src(paths.imagenes)
-        .pipe(cache(imagemin({ optimizationLevel: 3})))
-        .pipe(dest('public/build/img'))
-        .pipe(notify({ message: 'Imagen Completada'}));
+        .pipe( cache(imagemin({ optimizationLevel: 3})))
+        .pipe( dest('public/build/img'))
 }
 
-function versionWebp() {
-    return src(paths.imagenes)
-        .pipe( webp() )
-        .pipe(dest('public/build/img'))
-        .pipe(notify({ message: 'Imagen Completada'}));
+function versionWebp( done ) {
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest('public/build/img') )
+    done();
 }
 
+function versionAvif( done ) {
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( avif(opciones) )
+        .pipe( dest('public/build/img') )
+    done();
+}
 
-function watchArchivos() {
+function dev(done) {
     watch( paths.scss, css );
     watch( paths.js, javascript );
-    watch( paths.imagenes, imagenes );
-    watch( paths.imagenes, versionWebp );
+    watch( paths.imagenes, imagenes)
+    watch( paths.imagenes, versionWebp)
+    watch( paths.imagenes, versionAvif)
+    done()
 }
-  
+
 exports.css = css;
-exports.watchArchivos = watchArchivos;
-exports.default = parallel(css, javascript,  imagenes, versionWebp,  watchArchivos ); 
+exports.js = javascript;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel( css, imagenes, versionWebp, versionAvif, javascript, dev) ;
