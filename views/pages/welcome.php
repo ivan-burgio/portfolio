9401fa1160f34422e77ff425d339e5b16e37fdd0
ivan-burgio <ivan.24.burgio@gmail.com>
2023-08-20 16:29:09 -0300

    Fin de vista bienvenida

--- views/pages/welcome.php
+++ views/pages/welcome.php
@@ -1,16 +1,16 @@
 <header class="header">
     <nav class="header__nav">
-        <a href="/" class="header__enlaces">Inicio</a>
         <a href="/" class="header__enlaces">Sobre mi</a>
         <a href="/" class="header__enlaces">Proyectos</a>
         <a href="/" class="header__enlaces">Contacto</a>
+        <a href="/" class="header__enlaces">Admin</a>
     </nav>
 
     <div class="header__contenedor">
         <h2 class="header__titule">Welcome to my portfolio!</h2>
         <h1 class="header__name">I´m Iván Burgio</h1>
         <p class="header__iam">I´m a FullStack Web Development student who really enjoys learning about programming.</p>
-        <button class="header__btn">Start</button>
+        <button class="header__btn" id="btnStart">Start</button>
     </div>
 
 </header>
