9b55ab0f09167715eb9c69a4cd25400fa86f99bb
ivan-burgio <ivan.24.burgio@gmail.com>
2023-10-08 17:31:52 -0300

    Se añadio efecto de oscurecimiento en el welcome, lo que si, si se da para atras con el mouse, sigue estando todo negro

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+const btnStart = document.getElementById("btnStart");
+const overlay = document.getElementById("overlay");
+
+btnStart.addEventListener("click", (e) => {
+    e.preventDefault(); // Prevenir la redirección predeterminada
+    overlay.style.display = "block"; // Mostrar el overlay
+    setTimeout(() => {
+        overlay.style.opacity = "1"; // Hacer el overlay opaco
+        setTimeout(() => {
+            window.location.href = btnStart.href; // Redirigir después de que el overlay esté completamente visible
+        }, 1000); // Duración del efecto de opacidad (0.5 segundos en este caso)
+    }, 10);
+});
--- views/pages/welcome.php
+++ views/pages/welcome.php
@@ -5,6 +5,8 @@
             <span class="welcome__name">I'm Iván Burgio</span>
         </h1>
         <p class="welcome__iam">I'm a FullStack Web Development student who really enjoys learning about programming.</p>
+        
+        <div id="overlay"></div>
         <a class="welcome__btn" id="btnStart" href="intro">Start <i class="welcome__icon fa-solid fa-arrow-right"></i></a>
     </div>
 </header>
