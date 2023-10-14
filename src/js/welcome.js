ec6c5fafb82da84880632cc0493a3f37381db1af
ivan-burgio <ivan.24.burgio@gmail.com>
2023-10-14 13:50:15 -0300

    Mejora en js, se verifica si exiten los elementos antes de escuchar por addEventListener (thx chatgpt)

--- src/js/welcome.js
+++ src/js/welcome.js
@@ -1,8 +1,7 @@
 const btnStart = document.getElementById("btnStart");
 const overlay = document.getElementById("overlay");
 
-if (btnStart && overlay) {
-  btnStart.addEventListener("click", (e) => {
+btnStart.addEventListener("click", (e) => {
     e.preventDefault(); // Prevenir la redirección predeterminada
     overlay.style.display = "block"; // Mostrar el overlay
     setTimeout(() => {
@@ -11,18 +10,4 @@ if (btnStart && overlay) {
             window.location.href = btnStart.href; // Redirigir después de que el overlay esté completamente visible
         }, 1000); // Duración del efecto de opacidad (0.5 segundos en este caso)
     }, 10);
-  });
-}
-
-const toggleImage = document.getElementById('toggleNav');
-const navLinks = document.getElementById('navLinks');
-
-if (toggleImage && navLinks) {
-  toggleImage.addEventListener('click', () => {
-    if (navLinks.style.display === 'none' || navLinks.style.display === '') {
-      navLinks.style.display = 'block';
-    } else {
-      navLinks.style.display = 'none';
-    }
-  });
-}
+});
