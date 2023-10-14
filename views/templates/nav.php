a96ca558e0f494204b4efa5e09feebd37224dacd
ivan-burgio <ivan.24.burgio@gmail.com>
2023-10-14 13:39:44 -0300

    Mejoras abismales en el nav (sigue sin funcionar el codigo js)

--- views/templates/nav.php
+++ views/templates/nav.php
@@ -1,17 +1,22 @@
 <nav class="nav">
-  <div class="nav__contenedor">
-    <a href="/" class="nav__logo"></a>
-    <p class="nav__name">Iván Burgio</p>
-  </div>
-
-  <div class="nav__contenedor">
-    <div id="toggleNav" class="nav__toggle-image"></div>
-
-    <div id="navLinks" class="nav__links">
-      <a href="/aboutme" class="nav__link">About me</a>
-      <a href="/projects" class="nav__link">Projects</a>
-      <a href="/contact" class="nav__link">Contact</a>
-      <a href="/login" class="nav__link">Admin</a>
+    <div class="nav__contenedor-logo">
+        <a href="/" class="nav__logo"></a>
+        <p class="nav__name">Iván Burgio</p>
     </div>
-  </img>
+
+    <div class="nav__contenedor-links">
+        <div id="toggleNav" class="nav__toggle-image"></div>
+
+        <div id="navLinks" class="nav__links">
+            <div class="nav__contenedor-logo--toggle">
+                <a href="/" class="nav__logo"></a>
+                <p class="nav__name">Iván Burgio</p>
+            </div>
+
+            <a href="/aboutme" class="nav__link">About me</a>
+            <a href="/projects" class="nav__link">Projects</a>
+            <a href="/contact" class="nav__link">Contact</a>
+            <a href="/login" class="nav__link">Admin</a>
+        </div>
+    </div>
 </nav>
