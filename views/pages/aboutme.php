93a4850184ffa0bae31920c7e7af788161f7dbf8
ivan-burgio <ivan.24.burgio@gmail.com>
2023-11-07 16:56:25 -0300

    se creo el archivo para los estilos del aboutme

--- views/pages/aboutme.php
+++ views/pages/aboutme.php
@@ -1,12 +1,12 @@
 <main class="main aboutme">
-    <div data-aos="fade-up" class="intro__container intro__aboutme">
-        <p class="intro__aboutme-info">
+    <div data-aos="fade-up" class="aboutme__i">
+        <p class="aboutme__i-info">
             I am a passionate student of Web Development, with a solid background in technology. Currently, I am studying Computer Engineering and I actively participate in specialized training programs in the field of technology. My main objective is constant growth and continuous improvement of my programming skills, while I look for opportunities to enter the Information Technology (IT) sector.
             <br>
             <br>
             My fascination with programming in all its dimensions has led me to immerse myself in the world of Web Development, covering both FrontEnd and BackEnd. Additionally, I am spending time on additional courses to expand my knowledge and hone my skills in this exciting field.
         </p>
 
-        <img class="intro__aboutme-foto"></img>
+        <img class="aboutme__i-foto"></img>
     </div>
 </main>
