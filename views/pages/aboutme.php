454c7f27c28afc490926d9d2149774437937c74d
ivan-burgio <ivan.24.burgio@gmail.com>
2023-11-10 10:12:35 -0300

    logos de las tech

--- views/pages/aboutme.php
+++ views/pages/aboutme.php
@@ -17,42 +17,208 @@
             <div>
                 <p class="aboutme__tech-titules">FrontEnd</p>
                 <ul>
-                    <li class="aboutme__tech">HTML <span class="aboutme__tech-logo" id="aboutme__logo-html"></span></li>
-                    <li class="aboutme__tech">CSS <span class="aboutme__tech-logo" id="aboutme__logo-css"></span></li>
-                    <li class="aboutme__tech">SASS <span class="aboutme__tech-logo" id="aboutme__logo-sass"></span></li>
-                    <li class="aboutme__tech">JavaScript<span class="aboutme__tech-logo" id="aboutme__logo-js"></span></li>
-                    <li class="aboutme__tech">Bootsrap <span class="aboutme__tech-logo" id="aboutme__logo-bootsrap"></span></li>
-                    <li class="aboutme__tech">React <span class="aboutme__tech-logo" id="aboutme__logo-react"></span></li>
-                    <li class="aboutme__tech">Redux <span class="aboutme__tech-logo" id="aboutme__logo-redux"></span></li>
-                    <li class="aboutme__tech">NextJS <span class="aboutme__tech-logo" id="aboutme__logo-next"></span></li>
-                    <li class="aboutme__tech">Tailwind <span class="aboutme__tech-logo" id="aboutme__logo-tail"></span></li>
-                    <li class="aboutme__tech">Gatsby <span class="aboutme__tech-logo" id="aboutme__logo-gatsby"></span></li>
+                    <li class="aboutme__tech">HTML 
+                        <img 
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" 
+                            class="aboutme__tech-logo"
+                            alt="logo html"
+                        ></img>
+                    </li>
+
+                    <li class="aboutme__tech">CSS
+                        <img 
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" 
+                            class="aboutme__tech-logo"
+                            alt="logo css"
+                        ></img>
+                    </li>
+
+                    <li class="aboutme__tech">SASS 
+                        <img 
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg"
+                            class="aboutme__tech-logo"
+                            alt="logo sass"
+                        ></img>
+                    </li>
+
+                    <li class="aboutme__tech">JavaScript 
+                        <img 
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" 
+                            class="aboutme__tech-logo"
+                            alt="logo js"
+                        ></img>
+                    </li>
+
+                    <li class="aboutme__tech">Bootsrap 
+                        <img 
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"
+                            class="aboutme__tech-logo"
+                            alt="logo bootstrap"
+                        ></img>
+                    </li>
+
+                    <li class="aboutme__tech">React 
+                        <img 
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+                            class="aboutme__tech-logo"
+                            alt="logo react"
+                        ></img>
+                    </li>
+
+                    <li class="aboutme__tech">Redux 
+                        <img 
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" 
+                            class="aboutme__tech-logo"
+                            alt="logo redux"
+                        ></img>
+                    </li>
+
+                    <li class="aboutme__tech">NextJS 
+                        <img 
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg" 
+                            class="aboutme__tech-logo"
+                            alt="logo next"
+                        ></img>
+                    </li>
+
+                    <li class="aboutme__tech">Tailwind 
+                        <img 
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" 
+                            class="aboutme__tech-logo"
+                            alt="logo tailwind"
+                        ></img>
+                    </li>
+
+                    <li class="aboutme__tech">Gatsby 
+                        <img 
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gatsby/gatsby-plain.svg" 
+                            class="aboutme__tech-logo"
+                            alt="logo gatsby"
+                        ></img>
+                    </li>
                 </ul>
             </div>
 
             <div>
                 <p class="aboutme__tech-titules">BackEnd</p>
                 <ul>
-                    <li class="aboutme__tech">PHP <span class="aboutme__tech-logo" id="aboutme__logo-php"></span></li>
-                    <li class="aboutme__tech">SQL <span class="aboutme__tech-logo" id="aboutme__logo-sql"></span></li>
-                    <li class="aboutme__tech">MySQL <span class="aboutme__tech-logo" id="aboutme__logo-mysql"></span></li>
-                    <li class="aboutme__tech">NodeJS <span class="aboutme__tech-logo" id="aboutme__logo-node"></span></li>
+                    <li class="aboutme__tech">PHP 
+                        <img 
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg" 
+                            class="aboutme__tech-logo"
+                            alt="logo php"
+                        ></img>
+                    </li>
+
+                    <li class="aboutme__tech">SQL 
+                        <img 
+                            src="https://www.svgrepo.com/show/331760/sql-database-generic.svg" 
+                            class="aboutme__tech-logo"
+                            alt="logo sql"
+                        ></img>
+                    </li>
+
+                    <li class="aboutme__tech">MySQL 
+                        <img 
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg" 
+                            class="aboutme__tech-logo"
+                            alt="logo mysql"
+                        ></img>
+                    </li>
+
+                    <li class="aboutme__tech">NodeJS 
+                        <img 
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" 
+                            class="aboutme__tech-logo"
+                            alt="logo node"
+                        ></img>
+                    </li>
+
                 </ul>
             </div>
 
             <div>
                 <p class="aboutme__tech-titules">Development Tools</p>
                 <ul>
-                    <li class="aboutme__tech">GIT <span class="aboutme__tech-logo" id="aboutme__logo-git"></span></li>
-                    <li class="aboutme__tech">GitHub <span class="aboutme__tech-logo" id="aboutme__logo-github"></span></li>
-                    <li class="aboutme__tech">GulpJS <span class="aboutme__tech-logo" id="aboutme__logo-gulp"></span></li>
-                    <li class="aboutme__tech">WebPack <span class="aboutme__tech-logo" id="aboutme__logo-webpack"></span></li>
-                    <li class="aboutme__tech">NPM <span class="aboutme__tech-logo" id="aboutme__logo-npm"></span></li>
-                    <li class="aboutme__tech">Composer <span class="aboutme__tech-logo" id="aboutme__logo-composer"></span></li>
-                    <li class="aboutme__tech">Postman <span class="aboutme__tech-logo" id="aboutme__logo-postman"></span></li>
-                    <li class="aboutme__tech">Cypress <span class="aboutme__tech-logo" id="aboutme__logo-cypress"></span></li>
-                    <li class="aboutme__tech">Graphql <span class="aboutme__tech-logo" id="aboutme__logo-graphql"></span></li>
-                    <li class="aboutme__tech">Firebase <span class="aboutme__tech-logo" id="aboutme__logo-firebase"></span></li>
+                    <li class="aboutme__tech">GIT 
+                        <img 
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" 
+                            class="aboutme__tech-logo"
+                            alt="logo git"
+                        ></img>
+                    </li>
+
+                    <li class="aboutme__tech">GitHub 
+                        <img 
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" 
+                            class="aboutme__tech-logo"
+                            alt="logo github"
+                        ></img>
+                    </li>
+
+                    <li class="aboutme__tech">GulpJS 
+                        <img 
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gulp/gulp-plain.svg" 
+                            class="aboutme__tech-logo"
+                            alt="logo gulp"
+                        ></img>
+                    </li>
+
+                    <li class="aboutme__tech">WebPack 
+                        <img 
+                            src="https://raw.githubusercontent.com/webpack/media/master/logo/icon-square-big.png" 
+                            class="aboutme__tech-logo"
+                            alt="logo webpack"
+                        ></img>
+                    </li>
+
+                    <li class="aboutme__tech">NPM 
+                        <img 
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" 
+                            class="aboutme__tech-logo"
+                            alt="logo npm"
+                        ></img>
+                    </li>
+
+                    <li class="aboutme__tech">Composer 
+                        <img 
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/composer/composer-original.svg" 
+                            class="aboutme__tech-logo"
+                            alt="logo composer"
+                        ></img>
+                    </li>
+
+                    <li class="aboutme__tech">Postman 
+                        <img 
+                            src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" 
+                            class="aboutme__tech-logo"
+                            alt="logo postman"
+                        ></img>
+                    </li>
+
+                    <li class="aboutme__tech">Cypress 
+                        <img 
+                            src="https://openviewpartners.com/wp-content/uploads/2020/12/cypress-io-logo-round_-1.png" 
+                            class="aboutme__tech-logo"
+                            alt="logo cypress"
+                        ></img>
+                    </li>
+
+                    <li class="aboutme__tech">Graphql 
+                        <img 
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/graphql/graphql-plain.svg" 
+                            class="aboutme__tech-logo"
+                            alt="logo graph"
+                        ></img>
+                    </li>
+                    
+                    <li class="aboutme__tech">Firebase 
+                        <img 
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg" 
+                            class="aboutme__tech-logo"
+                            alt="logo firebase"
+                        ></img>
+                    </li>
                 </ul>
             </div>
         </div>
