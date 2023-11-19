886e8297892e9060b832a859d0bebea2b9c914d0
ivan-burgio <ivan.24.burgio@gmail.com>
2023-11-19 17:41:24 -0300

    Se agrego el texto descriptivo de los proyectos del 1 al tercero

--- views/pages/projects.php
+++ views/pages/projects.php
@@ -1,93 +1,92 @@
 <main class="main proyects">
-<?php proyects_template( // 2ª Proyecto React
-            'https://github.com/ivan-burgio/proyecto-control-gastos-vite',
-            '/build/img/proyecto_react2.png',
-            'This project is a site to keep track of your expenses, you can enter an initial amount and then add different expenses that you have to be able to keep precise control of your money.
+    <?php proyects_template( // 2ª Proyecto React
+        'https://github.com/ivan-burgio/proyecto-control-gastos-vite',
+        '/build/img/proyecto_react2.png',
+        'This project is a site to keep track of your expenses, you can enter an initial amount and then add different expenses that you have to be able to keep precise control of your money.
         Like all projects, it has field validation.
         You can also edit and delete some expenses and the budget value is updated in real time along with the graph.
         Personally, I consider it very useful to the point that I am going to use it in my daily life.'
-        ); ?>
+    ); ?>
 
-        <?php proyects_template( // 1ª Poryecto React
-            'https://github.com/ivan-burgio/proyecto-administrador-pacientes-vite',
-            '/build/img/proyecto_react1.png',
-            'This project is a simulation of a patient manager in a veterinary clinic.
+    <?php proyects_template( // 1ª Poryecto React
+        'https://github.com/ivan-burgio/proyecto-administrador-pacientes-vite',
+        '/build/img/proyecto_react1.png',
+        'This project is a simulation of a patient manager in a veterinary clinic.
         What makes this project special is that it has: <br>
         ✅Field validation to ensure that the information entered is correct.
         ✅Data persistence in localstorage to maintain a record of patients.
         ✅Patient editing and deletion functionality, making it a truly useful tool for patient management in a clinic.
         To build this project, I used Vite for the initial files, React for creating the interface, and TailwindCSS to give it a nice look. I also implemented Hooks like useEffect and useState, which are essential in developing React applications.'
-        ); ?>
+    ); ?>
 
-        <?php proyects_template( // 8ª Proyecto Desarrollo Web
-            'https://github.com/ivan-burgio/proyecto-devwebcamp-mvc',
-            '/build/img/proyecto_dw8.png',
-            'This site is a website to organize events.
+    <?php proyects_template( // 8ª Proyecto Desarrollo Web
+        'https://github.com/ivan-burgio/proyecto-devwebcamp-mvc',
+        '/build/img/proyecto_dw8.png',
+        'This site is a website to organize events.
         It has sections for account creation, login and recovery.
         It also has the creation of events, and speakers; You can view registered users with their lesson plan among other things in the administration section.
         It has alert systems to notify the user if data is missing or entered incorrectly and the security of the site includes URL protection, password hashing as shown in the video, sanitization of data to be entered, among other things.'
-        ); ?>
+    ); ?>
 
-        <?php proyects_template( // 7ª Proyecto Desarrollo Web
-            'https://github.com/ivan-burgio/proyecto-uptask-mvc',
-            '/build/img/proyecto_dw7.png',
-            'This site is a website to organize events.
+    <?php proyects_template( // 7ª Proyecto Desarrollo Web
+        'https://github.com/ivan-burgio/proyecto-uptask-mvc',
+        '/build/img/proyecto_dw7.png',
+        'This site is a website to organize events.
         It has sections for account creation, login and recovery.
         It also has the creation of events, and speakers; You can view registered users with their lesson plan among other things in the administration section.
         It has alert systems to notify the user if data is missing or entered incorrectly and the security of the site includes URL protection, password hashing as shown in the video, sanitization of data to be entered, among other things.'
-        ); ?>
+    ); ?>
 
-        <?php proyects_template( // 6ª Proyecto Desarrollo Web
-            'https://github.com/ivan-burgio/proyecto-appsalon-mvc',
-            '/build/img/proyecto_dw6.png',
-            'This site is a website to organize events.
+    <?php proyects_template( // 6ª Proyecto Desarrollo Web
+        'https://github.com/ivan-burgio/proyecto-appsalon-mvc',
+        '/build/img/proyecto_dw6.png',
+        'This site is a website to organize events.
         It has sections for account creation, login and recovery.
         It also has the creation of events, and speakers; You can view registered users with their lesson plan among other things in the administration section.
         It has alert systems to notify the user if data is missing or entered incorrectly and the security of the site includes URL protection, password hashing as shown in the video, sanitization of data to be entered, among other things.'
-        ); ?>
+    ); ?>
 
-        <?php proyects_template( // 5ª Proyecto Desarrollo Web
-            'https://github.com/ivan-burgio/proyecto-bienes-raices-mvc',
-            '/build/img/proyecto_dw5.png',
-            'This site is a website to organize events.
+    <?php proyects_template( // 5ª Proyecto Desarrollo Web
+        'https://github.com/ivan-burgio/proyecto-bienes-raices-mvc',
+        '/build/img/proyecto_dw5.png',
+        'This site is a website to organize events.
         It has sections for account creation, login and recovery.
         It also has the creation of events, and speakers; You can view registered users with their lesson plan among other things in the administration section.
         It has alert systems to notify the user if data is missing or entered incorrectly and the security of the site includes URL protection, password hashing as shown in the video, sanitization of data to be entered, among other things.'
-        ); ?>
+    ); ?>
 
-        <?php proyects_template( // 4ª Proyecto Desarrollo Web
-            'https://github.com/ivan-burgio/proyecto-festival-musica',
-            '/build/img/proyecto_dw4.png',
-            'This site is a website to organize events.
+    <?php proyects_template( // 4ª Proyecto Desarrollo Web
+        'https://github.com/ivan-burgio/proyecto-festival-musica',
+        '/build/img/proyecto_dw4.png',
+        'This site is a website to organize events.
         It has sections for account creation, login and recovery.
         It also has the creation of events, and speakers; You can view registered users with their lesson plan among other things in the administration section.
         It has alert systems to notify the user if data is missing or entered incorrectly and the security of the site includes URL protection, password hashing as shown in the video, sanitization of data to be entered, among other things.'
-        ); ?>
+    ); ?>
 
-        <?php proyects_template( // 3ª Proyecto Desarrollo Web
-            'https://github.com/ivan-burgio/proyecto-blog-cafe',
-            '/build/img/proyecto_dw3.png',
-            'This site is a website to organize events.
-        It has sections for account creation, login and recovery.
-        It also has the creation of events, and speakers; You can view registered users with their lesson plan among other things in the administration section.
-        It has alert systems to notify the user if data is missing or entered incorrectly and the security of the site includes URL protection, password hashing as shown in the video, sanitization of data to be entered, among other things.'
-        ); ?>
+    <?php proyects_template( // 3ª Proyecto Desarrollo Web
+        'https://github.com/ivan-burgio/proyecto-blog-cafe',
+        '/build/img/proyecto_dw3.png',
+        'Third project with the Web Development course.
+        This project sought to practice more CSS Grid, CSS FlexBox, Responsive and improve the Performance of a website as much as possible.
+        The project simulates a website where you can learn different ways of making coffee through a variety of courses.
+        The site also contains a library of JavaScript, Modernizr, and CSS Normalize.'
+    ); ?>
 
-        <?php proyects_template( // 2ª Proyecto Desarrollo Web
-            'https://github.com/ivan-burgio/proyecto-frontend-store',
-            '/build/img/proyecto_dw2.png',
-            'This site is a website to organize events.
-        It has sections for account creation, login and recovery.
-        It also has the creation of events, and speakers; You can view registered users with their lesson plan among other things in the administration section.
-        It has alert systems to notify the user if data is missing or entered incorrectly and the security of the site includes URL protection, password hashing as shown in the video, sanitization of data to be entered, among other things.'
-        ); ?>
+    <?php proyects_template( // 2ª Proyecto Desarrollo Web
+        'https://github.com/ivan-burgio/proyecto-frontend-store',
+        '/build/img/proyecto_dw2.png',
+        'Second web development project.
+        The purpose of this website was to learn more about Responsive, CSS Grid and CSS FlexBox among some other tools and properties.
+        The project simulates a virtual t-shirt store.
+        Each link to see each shirt is a different html file, so it is quite heavy and not very scalable.'
+    ); ?>
 
-        <?php proyects_template( // 1ª Proyecto Desarrollo Web
-            'https://github.com/ivan-burgio/proyecto-web-freelancer',
-            '/build/img/proyecto_dw1.png',
-            'This site is a website to organize events.
-        It has sections for account creation, login and recovery.
-        It also has the creation of events, and speakers; You can view registered users with their lesson plan among other things in the administration section.
-        It has alert systems to notify the user if data is missing or entered incorrectly and the security of the site includes URL protection, password hashing as shown in the video, sanitization of data to be entered, among other things.'
-        ); ?>
+    <?php proyects_template( // 1ª Proyecto Desarrollo Web
+        'https://github.com/ivan-burgio/proyecto-web-freelancer',
+        '/build/img/proyecto_dw1.png',
+        'Beginning of learning about Web Development. HTML and CSS were used. <br>
+        This site is my first project studying Web Development, it would be a type of simple Portfolio. <br>
+        I can not describe much more about this page since it was my first experience with these technologies and environment, so it is not even responsive.'
+    ); ?>
 </main>
