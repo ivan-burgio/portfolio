c1bb05e97a171c3b707959d4e8954dc7c1853e3d
ivan-burgio <ivan.24.burgio@gmail.com>
2023-11-21 11:28:08 -0300

    Mejora en estilos

--- views/pages/contact.php
+++ views/pages/contact.php
@@ -1,23 +1,23 @@
 <main class="main contact">
     <div class="contact__redes">
         <a class="contact__redes-link" href="https://github.com/ivan-burgio">
-            <i class="fa-brands fa-github" style="color: #000000"></i>
+            <i class="fa-brands fa-github"></i>
         </a>
         <a class="contact__redes-link" href="https://www.linkedin.com/in/ivan-burgio/">
-            <i class="fa-brands fa-linkedin" style="color: #000000"></i>
+            <i class="fa-brands fa-linkedin"></i>
         </a>
         <a class="contact__redes-link" href="https://www.instagram.com/ivan_burgio/">
-            <i class="fa-brands fa-instagram" style="color: #000000"></i>
+            <i class="fa-brands fa-instagram"></i>
         </a>
     </div>
 
     <form class="contact__form" id="formularioCorreo">
-        <label class="contact__label" for="mail">Mail</label>
-        <input class="contact__form-input" type="email" id="mail" name="mail" required>
+        <label class="contact__label" for="mail">Email <i class="fa-solid fa-envelope" style="color: #ffffff;"></i></label>
+        <input class="contact__form-input" placeholder="Your email" type="email" id="mail" name="mail" required>
 
         <div class="contact__form-mj" id="mensajeTextarea" style="display: none;">
-            <label class="contact__label" for="mensaje">Message</label>
-            <textarea class="contact__form-area" id="mensaje" name="mensaje" rows="4" cols="50" required readOnly></textarea>
+            <label class="contact__label" for="mensaje">Message <i class="fa-solid fa-keyboard" style="color: #ffffff;"></i></label>
+            <textarea class="contact__form-area" placeholder="Your message for me" id="mensaje" name="mensaje" rows="4" cols="50" required readOnly></textarea>
         </div>
 
         <div data-aos="fade-up" class="intro__moreinfo">
