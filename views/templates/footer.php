1622fee385272e1856ef1ab01839849b11c44415
Ivan Yang <119539386+Ivan-Yang25@users.noreply.github.com>
2023-11-25 14:16:03 -0300

    Update footer.php

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -1,4 +1,4 @@
 <footer class="footer">
         <div class="footer__logo"></div>
-        <p class="footer__titule">© Ivan Burgio, All Rights Reserved</p>
+        <p class="footer__titule">© Pingi Burgio, All Rights Reserved</p>
 </footer>
