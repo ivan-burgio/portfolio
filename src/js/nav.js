f80235d58b0cdc59ea821077f1283bc29923b56f
Iván Burgio <117323421+ivan-burgio@users.noreply.github.com>
2023-11-29 11:48:42 -0300

    Delete src directory

--- src/js/nav.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var toggleNav = document.getElementById("toggleNav");
-    var navLinks = document.getElementById("navLinks");
-
-    toggleNav.addEventListener("click", function () {
-        // Toggle the 'desplegado' class on navLinks
-        navLinks.classList.toggle("desplegado");
-    });
-});
