76f2eeca79924696da26073574ad890acc367965
ivan-burgio <ivan.24.burgio@gmail.com>
2023-11-29 11:33:06 -0300

    Se agrego meta description para mejorar SEO

--- views/pages-layout.php
+++ views/pages-layout.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Portfolio of Ivan Burgio, a programmer who studies Web Development, here you can learn a little more about him, the technologies he knows and his projects.">
     <title>Iván Burgio | <?php echo $titulo_pestaña ?> </title>
     <link rel="icon" type="image/png" href="/build/img/logo2.png"/>
     <link rel="preconnect" href="https://fonts.googleapis.com">
