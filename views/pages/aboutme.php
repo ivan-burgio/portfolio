510a464e589d9dd9ad11484bd8e6b5e44a8ba96f
ivan-burgio <ivan.24.burgio@gmail.com>
2023-12-12 12:54:55 -0300

    Optimizacion en la imagen de About me

--- views/pages/aboutme.php
+++ views/pages/aboutme.php
@@ -12,7 +12,11 @@
             My career began by studying Computer Engineering at the University, but after a few years I realized that I really didn't like it, so I was inclined to study on my own.
         </p>
 
-        <img class="aboutme__i-foto"></img>
+        <picture class="aboutme__i-foto">
+            <source type="image/avif" srcset="/build/img/yo.avif">
+            <source type="image/webp" srcset="/build/img/yo.webp">
+            <img loading="lazy" alt="image of me" src="/build/img/yo.png" loading="lazy">
+        </picture>
     </div>
 
     <div>
@@ -23,46 +27,46 @@
                 <p class="aboutme__tech-titules">FrontEnd</p>
                 <ul class="aboutme__tech-list">
                     <?php
-                        techs_template(
-                            'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg', 
-                            'HTML'
-                        );
-                        techs_template(
-                            'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg', 
-                            'CSS'
-                        );
-                        techs_template(
-                            'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg', 
-                            'SASS'
-                        );
-                        techs_template(
-                            'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg', 
-                            'JavaScript'
-                        );
-                        techs_template(
-                            'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg', 
-                            'Bootstrap'
-                        );
-                        techs_template(
-                            'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg', 
-                            'React'
-                        );
-                        techs_template(
-                            'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg', 
-                            'Redux'
-                        );
-                        techs_template(
-                            'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg', 
-                            'NextJS'
-                        );
-                        techs_template(
-                            'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg', 
-                            'Tailwind'
-                        );
-                        techs_template(
-                            'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gatsby/gatsby-plain.svg', 
-                            'Gatsby'
-                        );
+                    techs_template(
+                        'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
+                        'HTML'
+                    );
+                    techs_template(
+                        'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg',
+                        'CSS'
+                    );
+                    techs_template(
+                        'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg',
+                        'SASS'
+                    );
+                    techs_template(
+                        'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+                        'JavaScript'
+                    );
+                    techs_template(
+                        'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg',
+                        'Bootstrap'
+                    );
+                    techs_template(
+                        'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+                        'React'
+                    );
+                    techs_template(
+                        'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg',
+                        'Redux'
+                    );
+                    techs_template(
+                        'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg',
+                        'NextJS'
+                    );
+                    techs_template(
+                        'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg',
+                        'Tailwind'
+                    );
+                    techs_template(
+                        'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gatsby/gatsby-plain.svg',
+                        'Gatsby'
+                    );
                     ?>
                 </ul>
             </div>
@@ -71,70 +75,70 @@
                 <p class="aboutme__tech-titules">BackEnd</p>
                 <ul class="aboutme__tech-list">
                     <?php
-                        techs_template(
-                            'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg', 
-                            'PHP'
-                        );
-                        techs_template(
-                            'https://www.svgrepo.com/show/331760/sql-database-generic.svg', 
-                            'SQL'
-                        );
-                        techs_template(
-                            'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg', 
-                            'MySQL'
-                        );
-                        techs_template(
-                            'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg', 
-                            'NodeJS'
-                        );
+                    techs_template(
+                        'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg',
+                        'PHP'
+                    );
+                    techs_template(
+                        'https://www.svgrepo.com/show/331760/sql-database-generic.svg',
+                        'SQL'
+                    );
+                    techs_template(
+                        'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg',
+                        'MySQL'
+                    );
+                    techs_template(
+                        'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg',
+                        'NodeJS'
+                    );
                     ?>
                 </ul>
             </div>
 
             <div data-aos="fade-up" class="aboutme__tech-section">
                 <p class="aboutme__tech-titules">Development Tools</p>
-                <ul class="aboutme__tech-list" >
+                <ul class="aboutme__tech-list">
                     <?php
-                        techs_template(
-                            'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg', 
-                            'GIT'
-                        );
-                        techs_template(
-                            'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg', 
-                            'GitHub'
-                        );
-                        techs_template(
-                            'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gulp/gulp-plain.svg', 
-                            'GulpJS'
-                        );
-                        techs_template(
-                            'https://raw.githubusercontent.com/webpack/media/master/logo/icon-square-big.png', 
-                            'WebPack'
-                        );
-                        techs_template(
-                            'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg', 
-                            'NPM'
-                        );
-                        techs_template(
-                            'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/composer/composer-original.svg', 
-                            'Composer'
-                        );
-                        techs_template(
-                            'https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg', 
-                            'Postman'
-                        );
-                        techs_template(
-                            'https://openviewpartners.com/wp-content/uploads/2020/12/cypress-io-logo-round_-1.png', 
-                            'Cypress'
-                        );
-                        techs_template(
-                            'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/graphql/graphql-plain.svg', 
-                            'Graphql'
-                        );
-                        techs_template(
-                            'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg', 
-                            'Firebase'
-                        );
+                    techs_template(
+                        'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg',
+                        'GIT'
+                    );
+                    techs_template(
+                        'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg',
+                        'GitHub'
+                    );
+                    techs_template(
+                        'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gulp/gulp-plain.svg',
+                        'GulpJS'
+                    );
+                    techs_template(
+                        'https://raw.githubusercontent.com/webpack/media/master/logo/icon-square-big.png',
+                        'WebPack'
+                    );
+                    techs_template(
+                        'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg',
+                        'NPM'
+                    );
+                    techs_template(
+                        'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/composer/composer-original.svg',
+                        'Composer'
+                    );
+                    techs_template(
+                        'https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg',
+                        'Postman'
+                    );
+                    techs_template(
+                        'https://openviewpartners.com/wp-content/uploads/2020/12/cypress-io-logo-round_-1.png',
+                        'Cypress'
+                    );
+                    techs_template(
+                        'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/graphql/graphql-plain.svg',
+                        'Graphql'
+                    );
+                    techs_template(
+                        'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg',
+                        'Firebase'
+                    );
                     ?>
                 </ul>
             </div>
